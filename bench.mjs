432371aafd222b6497cec7f912e8af87d5278c75
Staś Małolepszy <stas@mozilla.com>
2018-04-27 17:51:08 +0200

    Benchmark gecko-strings

--- bench.mjs
+++ bench.mjs
@@ -1,6 +1,8 @@
-
+import fs from "fs";
 import perf from "perf_hooks";
 const { PerformanceObserver, performance } = perf;
+import FluentSyntax from "fluent-syntax";
+import FluentRuntime from "fluent";
 
 import fluent from "./grammar.mjs";
 
@@ -10,31 +12,20 @@ new PerformanceObserver(items => {
     performance.clearMarks();
 }).observe({entryTypes: ['measure']});
 
-const ftl = `
-lipsum =
-    Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse
-    a pellentesque dui, non felis. Maecenas malesuada elit lectus felis,
-    malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta
-    urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus
-    fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet
-    magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit,
-    rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum.
-    Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id,
-    eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus
-    non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere
-    cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis
-    natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida
-    vitae, dapibus risus ante sodales lectus blandit eu, tempor diam pede
-    cursus vitae, ultricies eu, faucibus quis, porttitor eros cursus lectus,
-    pellentesque eget, bibendum a, gravida ullamcorper quam. Nullam viverra
-    consectetuer. Quisque cursus et, porttitor risus. Aliquam sem. In hendrerit
-    nulla quam nunc, accumsan congue. Lorem ipsum primis in nibh vel risus. Sed
-    vel lectus. Ut sagittis, ipsum dolor quam.
-`;
+const ftl = fs.readFileSync("gecko-strings.ftl", "utf8");
 
-performance.mark('start parse');
-fluent.run(ftl).fold(
-    () => console.log("Parsing successful."),
-    () => console.error("Parsing failed."));
-performance.mark('end parse');
-performance.measure('Parse time', 'start parse', 'end parse');
+performance.mark('start1');
+fluent.run(ftl);
+performance.mark('end1');
+performance.measure("fluent-kombi", 'start1', 'end1');
+
+performance.mark('start2');
+FluentSyntax.parse(ftl);
+performance.mark('end2');
+performance.measure("fluent-syntax", 'start2', 'end2');
+
+performance.mark('start3');
+FluentRuntime._parse(ftl);
+performance.mark('end3');
+performance.measure("fluent (runtime)", 'start3', 'end3');
+
