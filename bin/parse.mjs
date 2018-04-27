0e8a02b3804a1c439488041fc9031fe22bdf5656
Staś Małolepszy <stas@mozilla.com>
2018-04-27 14:07:13 +0200

    Add bin/parse

---
+++ bin/parse.mjs
@@ -0,0 +1,18 @@
+import readline from "readline";
+import fluent from "../";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "fluent>",
+});
+
+const lines = [];
+
+rl.on("line", line => lines.push(line));
+rl.on("close", () => {
+    const ftl = lines.join("\n");
+    fluent.run(ftl).fold(
+        ast => console.log(JSON.stringify(ast, null, 4)),
+        err => console.error(err));
+});
