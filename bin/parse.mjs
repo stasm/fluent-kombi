87c7013aaf5d5e2a871c4178ea63cb8a7c3c4c32
Staś Małolepszy <stas@mozilla.com>
2018-05-10 14:12:13 +0200

    Make bin/parse handle file paths

--- bin/parse.mjs
+++ bin/parse.mjs
@@ -1,18 +1,74 @@
+import fs from "fs";
 import readline from "readline";
+import parse_args from "minimist";
 import fluent from "../";
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "fluent>",
+const argv = parse_args(process.argv.slice(2), {
+    boolean: ["help"],
+    alias: {
+        help: "h",
+    },
 });
 
-const lines = [];
+if (argv.help) {
+    exit_help(0);
+}
 
-rl.on("line", line => lines.push(line));
-rl.on("close", () => {
-    const ftl = lines.join("\n") + "\n";
+const [file_path] = argv._;
+
+if (file_path === "-") {
+    parse_stdin();
+} else if (file_path) {
+    parse_file(file_path);
+} else {
+    exit_help(1);
+}
+
+function exit_help(exit_code) {
+    console.log(`
+    Usage: node --experimental-modules parse.mjs [OPTIONS] <FILE>
+
+    When FILE is "-", read text from stdin.
+
+    Examples:
+
+        node --experimental-modules parse.mjs path/to/file.ftl
+        cat path/to/file.ftl | node --experimental-modules parse.mjs -
+
+    Options:
+
+        -h, --help      Display help and quit.
+`);
+    process.exit(exit_code);
+}
+
+function parse_stdin() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "fluent>",
+    });
+
+    const lines = [];
+
+    rl.on("line", line => lines.push(line));
+    rl.on("close", () =>
+        parse(lines.join("\n") + "\n"));
+}
+
+function parse_file(file_path) {
+    fs.readFile(file_path, "utf8", (err, content) => {
+        if (err) {
+            throw err;
+        }
+
+        parse(content);
+    });
+}
+
+
+function parse(ftl) {
     fluent.run(ftl).fold(
         ast => console.log(JSON.stringify(ast, null, 4)),
         err => console.error(err));
-});
+}
