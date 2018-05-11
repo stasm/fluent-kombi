7819e991e075f64c9e68e38fa02f81ebfcfb02d3
Staś Małolepszy <stas@mozilla.com>
2018-05-11 14:30:12 +0200

    EBNF: Ensure precedence of operators

--- ebnf/serializer.mjs
+++ ebnf/serializer.mjs
@@ -18,79 +18,74 @@ function serialize_expression(expression, state) {
 }
 
 function serialize_operator({name, args}, state) {
-    let serialize = expr =>
-        serialize_expression(expr, {...state, parent: name});
+    let serialized_args = args.map(arg =>
+        serialize_expression(arg, {...state, parent: name}));
+
+    function ensure_prec(text) {
+        return state.parent ? `(${text})` : text;
+    }
 
     switch (name) {
         case "after": {
-            return args.map(serialize).join(" ");
+            return ensure_prec(
+                serialized_args.join(" "));
         }
         case "always": {
             return null;
         }
         case "and": {
-            let operands = args.map(serialize).reverse().join(" ");
-            return state.parent ? `(${operands})` : operands;
+            return ensure_prec(
+                serialized_args.reverse().join(" "));
         }
         case "between": {
-            let [delim, expr] = args.map(serialize);
-            return `${delim} ${expr} ${delim}`;
+            let [delim, expr] = serialized_args;
+            return ensure_prec(`${delim} ${expr} ${delim}`);
         }
         case "char": {
-            let [arg] = args.map(serialize);
-            return `"${arg}"`;
+            return `"${serialized_args[0]}"`;
         }
         case "charset": {
-            let [arg] = args.map(serialize);
-            return `[${arg}]`;
+            return `[${serialized_args[0]}]`;
         }
         case "either": {
-            let operands = args.map(serialize);
             if (state.parent) {
-                return `(${operands.join(" | ")})`;
+                return `(${serialized_args.join(" | ")})`;
             }
 
             // Add 5 to align with "<rule name> ::= ".
             let padding = state.max_name_length + 5;
-            return operands.join("\n" + " | ".padStart(padding));
+            return serialized_args.join("\n" + " | ".padStart(padding));
         }
         case "eof": {
             return "EOF";
         }
         case "maybe": {
-            let [arg] = args.map(serialize);
-            return `${arg}?`;
+            return `${serialized_args[0]}?`;
         }
         case "maybe": {
-            let [arg] = args.map(serialize);
-            return `${arg}?`;
+            return `${serialized_args[0]}?`;
         }
         case "never": {
             return null;
         }
         case "not": {
-            let [arg] = args.map(serialize);
-            return `- ${arg}`;
+            return `- ${serialized_args[0]}`;
         }
         case "regex": {
-            let [arg] = args.map(serialize);
-            return `/${arg}/`;
+            return `/${serialized_args[0]}/`;
         }
         case "repeat": {
-            let [arg] = args.map(serialize);
-            return `${arg}*`;
+            return `${serialized_args[0]}*`;
         }
         case "repeat1": {
-            let [arg] = args.map(serialize);
-            return `${arg}+`;
+            return `${serialized_args[0]}+`;
         }
         case "sequence": {
-            let operands = args.map(serialize).filter(Boolean).join(" ");
-            return state.parent ? `(${operands})` : operands;
+            return ensure_prec(
+                serialized_args.filter(Boolean).join(" "));
         }
         case "string": {
-            let [arg] = args.map(serialize);
-            return `"${arg}"`;
+            return `"${serialized_args[0]}"`;
         }
     }
 }
